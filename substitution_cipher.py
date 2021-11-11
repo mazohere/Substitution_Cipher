cbaa376cb1dc4ae3312240536c077f05dd630553
mazohere <83385567+mazohere@users.noreply.github.com>
2021-11-11 21:55:02 +1100

    some stuff changed not that much though

--- substitution_cipher.py
+++ substitution_cipher.py
@@ -4,8 +4,21 @@ print(first.read)
 original = first.read()
 
 # convters the text into users choice
+def converter(sub_num):
+    # use the lambda thning
+    print('i dont want any errors so im printing this')
+
+def user_input():
+    print('how many letters away would you like the cipher to be: ')
+    choice = input(' ')
+
+    if isinstance(choice, int) == True:
+        converter()
+    elif isinstance(choice, int) == False:
+        print('incorrect input, please try again')
+        user_input(choice)
+
 
 # writes the converted text into the write file and removes any existing text from the file
-
 converted = open('write.txt', 'w')
 converted.write(original)
